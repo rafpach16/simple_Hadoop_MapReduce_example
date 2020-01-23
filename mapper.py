4506ea2293ee2598005b91d69bf63de64f0c8c07
rafpach16 <rpacheco002@regis.edu>
2020-01-23 05:05:55 +0000

    remove stopwords,lowercase, and punctuation

--- mapper.py
+++ mapper.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
-import sys
+import string,  sys
 
-# get all lines from stdin
+from sklearn.feature_extraction import stop_words
+
+stops = set(stop_words.ENGLISH_STOP_WORDS)
+
 for line in sys.stdin:
-    # remove leading and trailing whitespace
+    ## removes the whitespace
     line = line.strip()
 
-    # split the line into words; splits on any whitespace
+    ## removes punctuation
+    line = line.translate( string.maketrans(string.punctuation, ' ' * len(string.punctuation)) )
+
+    ## makes sure that the text is a valid Unicode string
+    ## ignores the characters that are not valid
+ 
+
+    ## converts the text to lowercase 
+    line = line.lower()
+
+    ## this splits words at all whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
-    for word in words:
-        print '%s\t%s' % (word, "1")
+    ## prints out all the words with a count of 1
+    for w in words:
+       if w not in stops:
+         print '%s\t%s' % (w, "1")
+
+
